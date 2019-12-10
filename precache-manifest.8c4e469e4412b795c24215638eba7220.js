a2a41e6bbb5dc418316771e54b0948cf1f8902e1
noval <novalmaulana@gmail.com>
2019-12-10 11:25:21 +0700

    react test

---
+++ precache-manifest.8c4e469e4412b795c24215638eba7220.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a41fe42f64bcfc899cda9041e0642a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3cd432ad66b61a1b06b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4bff26d4e64677685905",
+    "url": "/static/js/2.338553a3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.338553a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3cd432ad66b61a1b06b",
+    "url": "/static/js/main.4f233840.chunk.js"
+  },
+  {
+    "revision": "12be67df5175e671986e",
+    "url": "/static/js/runtime-main.7d800f9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
